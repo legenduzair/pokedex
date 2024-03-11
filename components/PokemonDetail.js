1855fd8c93427afb1d05274b9cea811a7e74693d
Uzair Oomer <uzair.oomer@jdplc.com>
2024-03-11 13:15:03 +0000

    clean up UI of pokemon details page by adding tabs and stats

--- components/PokemonDetail.js
+++ components/PokemonDetail.js
@@ -1,5 +1,5 @@
 "use client";
-import { Box, Card, Flex } from "@radix-ui/themes";
+import { Box, Card, Flex, Tabs, Text } from "@radix-ui/themes";
 import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
@@ -57,7 +57,9 @@ const PokemonDetail = ({ params }) => {
         className="my-32 mx-5 w-[90%] md:w-[80%] lg:w-[70%] h-full"
       >
         <Link href="/">
-          <small><span className="pr-2">&larr;</span> Back to the 151 Pokemon</small>
+          <small>
+            <span className="pr-2">&larr;</span> Back to the 151 Pokemon
+          </small>
         </Link>
         <Box
           className="flex flex-col gap-2 items-center mt-10 sm:items-start sm:flex-row sm:gap-5 md:gap-10"
@@ -74,35 +76,50 @@ const PokemonDetail = ({ params }) => {
             <p className="pb-3">{pokemonDetails.name}</p>
             <p className="pb-3">{joinTypesWithSlash(pokemonDetails.types)}</p>
             <Description pokemonDetails={pokemonDetails} />
-            <div className="flex gap-20 ">
+            <div>
               <div>
-                <h1>Stats:</h1>
                 <p>Height: {pokemonDetails.height / 10}m</p>
                 <p>Weight: {pokemonDetails.weight / 10}kg</p>
-              </div>
-              <div>
-                <h1>Abilities:</h1>
-                <ul>
-                  {pokemonDetails.abilities.map((ability, index) => (
-                    <li key={index}>{ability.ability.name}</li>
-                  ))}
-                </ul>
               </div>
             </div>
           </Box>
         </Box>
         <Box>
           <div className="pt-10 capitalize">
-            <h1>Move List:</h1>
-            <div className="move-list">
-              {formatMoveList(pokemonDetails.moves)
-                .split(", ")
-                .map((move, index) => (
-                  <span key={index} className="move">
-                    {move}
-                  </span>
-                ))}
-            </div>
+            <Tabs.Root defaultValue="abilities">
+              <Tabs.List>
+                <Tabs.Trigger value="abilities">Abilities</Tabs.Trigger>
+                <Tabs.Trigger value="stats">Stats</Tabs.Trigger>
+                <Tabs.Trigger value="move-list">Move List</Tabs.Trigger>
+              </Tabs.List>
+              <Box px="4" pt="3" pb="2">
+                <Tabs.Content value="abilities">
+                  <div>
+                    <ul>
+                      {pokemonDetails.abilities.map((ability, index) => (
+                        <li className="pb-1" key={index}>{ability.ability.name}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </Tabs.Content>
+                <Tabs.Content value="stats">
+                  {pokemonDetails.stats.map((stat, index) => (
+                    <Text className="flex pb-1">{stat.stat.name}: {stat.base_stat}</Text>
+                  ))}
+                </Tabs.Content>
+                <Tabs.Content value="move-list">
+                  <div className="move-list">
+                    {formatMoveList(pokemonDetails.moves)
+                      .split(", ")
+                      .map((move, index) => (
+                        <span key={index} className="move">
+                          {move}
+                        </span>
+                      ))}
+                  </div>
+                </Tabs.Content>
+              </Box>
+            </Tabs.Root>
           </div>
         </Box>
       </Card>
